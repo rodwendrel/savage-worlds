83893947bea0f9d0972e4e5bb0197cd09e2e0900
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2023-01-25 00:55:55 -0300

    route

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Hammer from './Sledgehammer';
+
 function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/sledge' element={<Hammer />} />
+      </Routes>
     </div>
   )
 }
