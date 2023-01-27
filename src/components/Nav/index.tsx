3f89b771650a4ae4df41cf3358b8773f4aaa28ea
Rodrigo Ferreira <rodrigo.wendrel@protonmail.com>
2023-01-27 17:30:51 -0300

    Dynamic Cards

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,10 +1,28 @@
 import * as S from './styles'
+import { Card } from '../../components/Card';
+import { Card2 } from '../../components/Card';
+import {useState} from 'react';
+
 
 const Nav = () => {
+    const [active, setActive] = useState("Card")
+
+
+
     return <S.Nav>
         <nav>
             <div className="list">
-            <h3>Bem vindo, Senhor Arthur Holtz</h3>
+                <h3>Bem vindo, Senhor Arthur Holtz</h3>
+
+                <ul>
+                    <li><button onClick={() => setActive("Card")}>Card</button></li>
+                    <li><button onClick={() => setActive("Card2")}>Card2</button></li>
+                </ul>
+
+                {active === "Card"  && <Card title='sd' text='dafsdfdf'/>}
+                {active === "Card2" && <Card2 title='sdfsdfsf' text='dafsdfdf'/>}
+
+
             </div>
         </nav>
     </S.Nav>
--- src/pages/Sledgehammer/index.tsx
+++ src/pages/Sledgehammer/index.tsx
@@ -1,15 +1,15 @@
 import * as S from './styles';
 import Stars from '../../components/Stars';
 import Nav from '../../components/Nav';
-import { Cards } from '../Cards';
+
+
 
 const Hammer = () => {
 
     return <S.Hammer>
         <Stars />
         <Nav />
-        <Cards />
-
+        
     </S.Hammer>
 }
 
